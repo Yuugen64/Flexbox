5c265fcb2cb1c64582bee7fd4c6243ccd1aeedaf
Yuugen64 <yuugen64@protonmail.com>
2024-08-21 18:16:57 -0500

    [INITIAL]:: adding the project skeleton to test on mobile device.

---
+++ scripts/app.js
@@ -0,0 +1,3 @@
+import { lexicon } from './text.js';
+
+console.log(lexicon[0].greet1);
---
+++ scripts/text.js
@@ -0,0 +1,20 @@
+export const lexicon = [
+    {
+        id: 0, 
+        category: "greeting", 
+        greet1: "Hi how are you today?"
+    },
+
+    {
+        id: 1, 
+        category: "greet-resp", 
+        resp1: "I'm glad you are well!", 
+        resp2: "I'm glad you are well!"
+    },
+
+    {
+        id: 2, 
+        category: "greeting", 
+        greet1: "Hi how are you today?"
+    },
+];
